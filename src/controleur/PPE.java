3a60999a8b9a5d8a332ac7b414b4887b4b2b8191
Louis-2204 <louis.lamouche2204@gmail.com>
2023-02-04 16:29:59 +0100

    Menu principal

--- src/controleur/PPE.java
+++ src/controleur/PPE.java
@@ -1,10 +1,34 @@
 package controleur;
 
+import java.util.Scanner;
+
 public class PPE {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		int choix = 0;
+		Scanner sc = new Scanner(System.in);
+		do {
+			System.out.println("______Menu Auto-École______");
+			System.out.println("1- Gestion des élèves");
+			System.out.println("2- Gestion des moniteurs");
+			System.out.println("3- Gestion des véhicules");
+			System.out.println("4- Gestion des formules");
+			System.out.println("0- Quitter");
+			System.out.println("Votre choix :");
+			choix = sc.nextInt();
+			switch (choix) {
+				case 1:
+					C_Eleve.menuEleve();
+					break;
+				case 2:
+					break;
+				case 3:
+					C_Vehicule.menuVehicule();
+					break;
+				case 4:
+					break;
+			}
+		} while (choix != 0);
 	}
-
 }
